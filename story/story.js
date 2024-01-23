d16e165547d15ac9d9f300a2620da842a482d6bb
MayaIvimey <146374490+MayaIvimey@users.noreply.github.com>
2024-01-22 21:46:56 -0500

    Update story.js

--- story/story.js
+++ story/story.js
@@ -1,6 +1,12 @@
-function initMap() {
-  const map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 3,
-    center: { lat: 49.24595168423854, lng: -83.16636863826454 }, 
+let map;
+
+async function initMap() {
+  const { Map } = await google.maps.importLibrary("maps");
+
+  map = new Map(document.getElementById("map"), {
+    center: { lat: -34.397, lng: 150.644 },
+    zoom: 8,
   });
 }
+
+initMap();
