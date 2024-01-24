edb30deff77e1e0c2da06b7f4f866deb80746d61
MayaIvimey <146374490+MayaIvimey@users.noreply.github.com>
2024-01-24 18:37:15 -0500

    Update story.js

--- story/story.js
+++ story/story.js
@@ -1,15 +1,11 @@
 
 
 function initMap() {
-  var mapOptions = {
-    center: { lat: 44.226000090936395, lng: -76.49499549568613 }, 
-    zoom: 16,
-  };
 
-const map = new google.maps.Map(document.getElementById('map'),{
+ const map = new google.maps.Map(document.getElementById("map"), {
     center: { lat: 44.226000090936395, lng: -76.49499549568613 }, 
     zoom: 16,
-    mapTypeId: "terrain",
+    mapTypeId: "satellite",
 });
 
 }
