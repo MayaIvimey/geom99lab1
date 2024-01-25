5d513b6e9b1cdd3c707c407126e8b04b019ad320
MayaIvimey <146374490+MayaIvimey@users.noreply.github.com>
2024-01-25 10:21:57 -0500

    Update story.js

--- story/story.js
+++ story/story.js
@@ -8,6 +8,52 @@ function initMap() {
   };
   var map = new google.maps.Map(document.getElementById('map'),
     mapOptions);
+
+  const locations = [
+    {
+      position: { lat: 44.224455413902966, lng: -76.51526780630265},
+      title: "Jean Royce",
+    },
+    {
+      position: { lat: 44.224407319071936, lng: -76.50071114448876},
+      title: "Leonard Hall",
+    },
+    {
+      position: { lat: 44.22466084361948, lng: -76.49622990388815 },
+      title: "Ban Righ Hall",
+    },
+    {
+      position: { lat: 44.223620329676336, lng: -76.4992960158313 },
+      title: "Location 21",
+    },
+    {
+      position: { lat: 44.22535274880164, lng: -76.49863267430514 },
+      title: "The Lazy Scholar",
+    },
+  ];
+  const infoWindow = new InfoWindow();
+  locations.forEach(({ position, title }, i) => {
+    const pin = new PinElement({
+      glyph: `${i + 1}`,
+    });
+    const marker = new AdvancedMarkerElement({
+      position,
+      map,
+      title: `${i + 1}. ${title}`,
+      content: pin.element,
+    });
+  marker.addListener("click", ({ domEvent, latLng }) => {
+      const { target } = domEvent;
+
+      infoWindow.close();
+      infoWindow.setContent(marker.title);
+      infoWindow.open(marker.map, marker);
+    });
+  });
+
+
+
+  
 /* styler takes off POIs, makes the map less busy and shows the Queens restaurants better. Code comes from https://developers.google.com/maps/documentation/javascript/style-reference
 and https://stackoverflow.com/questions/3110020/google-maps-api-v3-no-labels*/
   var customStyled = [{
@@ -17,14 +63,6 @@ and https://stackoverflow.com/questions/3110020/google-maps-api-v3-no-labels*/
       { visibility: "off" }
     ]
     }];
-  map.set('styles',customStyled);
-new google.maps.Marker({
-    position: {lat: 44.22901925950561, lng:-76.49484263870534},
-    map,
-    title: "Common Ground",
-  });
- 
+  map.set('styles',customStyled); 
 }
-  
-
-
+initMap();
