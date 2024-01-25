a571598526b4956070c9276f30d16ecd7474b974
MayaIvimey <146374490+MayaIvimey@users.noreply.github.com>
2024-01-25 11:11:26 -0500

    Update story.js

--- story/story.js
+++ story/story.js
@@ -19,26 +19,16 @@ and https://stackoverflow.com/questions/3110020/google-maps-api-v3-no-labels*/
 
   
   const locations = [
-    {
-      position: { lat: 44.224455413902966, lng: -76.51526780630265},
-      title: "Jean Royce",
-    },
-    {
-      position: { lat: 44.224407319071936, lng: -76.50071114448876},
-      title: "Leonard Hall",
-    },
-    {
-      position: { lat: 44.22466084361948, lng: -76.49622990388815 },
-      title: "Ban Righ Hall",
-    },
-    {
-      position: { lat: 44.223620329676336, lng: -76.4992960158313 },
-      title: "Location 21",
-    },
-    {
-      position: { lat: 44.22535274880164, lng: -76.49863267430514 },
-      title: "The Lazy Scholar",
-    },
+    [{ lat: 44.224455413902966, lng: -76.51526780630265},
+      "Jean Royce"],
+    [{ lat: 44.224407319071936, lng: -76.50071114448876},
+      "Leonard Hall"],
+    [{ lat: 44.22466084361948, lng: -76.49622990388815 },
+      "Ban Righ Hall"],
+    [{ lat: 44.223620329676336, lng: -76.4992960158313 },
+      "Location 21"],
+    [{ lat: 44.22535274880164, lng: -76.49863267430514 },
+      "The Lazy Scholar"],
   ];
   const infoWindow = new InfoWindow();
   locations.forEach(([position, title], i) => {
