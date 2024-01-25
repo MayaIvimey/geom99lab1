a65db08a1bf1539feca2e545979fc728c72ecf75
MayaIvimey <146374490+MayaIvimey@users.noreply.github.com>
2024-01-24 23:37:03 -0500

    Update story.js

--- story/story.js
+++ story/story.js
@@ -1,8 +1,5 @@
 function initMap() {
-  const { Map, InfoWindow } = await google.maps.importLibrary("maps");
-  const { AdvancedMarkerElement, PinElement } = await google.maps.importLibrary(
-    "marker",
-  );
+
   
   var myLatlng = new google.maps.LatLng(44.226000090936395, -76.49499549568613);
   var mapOptions = {
@@ -21,7 +18,28 @@ and https://stackoverflow.com/questions/3110020/google-maps-api-v3-no-labels*/
     ]
     }];
   map.set('styles',customStyled);
-
+  const food = [
+    {
+      position: { lat: 44.224455413902966, lng: -76.51526780630265},
+      title: "Jean Royce",
+    },
+    {
+      position: { lat: 44.224407319071936, lng: -76.50071114448876},
+      title: "Leonard Hall",
+    },
+    {
+      position: { lat: 44.22466084361948, lng: -76.49622990388815 },
+      title: "Ban Righ Hall",
+    },
+    {
+      position: { lat: 44.223620329676336, lng: -76.4992960158313 },
+      title: "Location 21",
+    },
+    {
+      position: { lat: 44.22535274880164, lng: -76.49863267430514 },
+      title: "The Lazy Scholar",
+    },
+  ];
 
 
   
