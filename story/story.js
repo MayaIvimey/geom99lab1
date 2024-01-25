0b441499c11de68b0835a0cff1394a9a13d6a64e
MayaIvimey <146374490+MayaIvimey@users.noreply.github.com>
2024-01-24 22:15:44 -0500

    Update story.js

--- story/story.js
+++ story/story.js
@@ -1,24 +1,22 @@
 function initMap() {
-
-
   
-var myLatlng = new google.maps.LatLng(44.226000090936395, -76.49499549568613);
-var mapOptions = {
+  var myLatlng = new google.maps.LatLng(44.226000090936395, -76.49499549568613);
+  var mapOptions = {
   zoom: 16,
   center: myLatlng,
-};
-var map = new google.maps.Map(document.getElementById('map'),
+  };
+  var map = new google.maps.Map(document.getElementById('map'),
     mapOptions);
 /* styler takes off POIs, makes the map less busy and shows the Queens restaurants better. Code comes from https://developers.google.com/maps/documentation/javascript/style-reference
 and https://stackoverflow.com/questions/3110020/google-maps-api-v3-no-labels*/
-var customStyled = [{
+  var customStyled = [{
     featureType: "poi.business",
     elementType: "labels",
     stylers: [
       { visibility: "off" }
     ]
-  }];
-map.set('styles',customStyled);
+    }];
+  map.set('styles',customStyled);
 
 
 
