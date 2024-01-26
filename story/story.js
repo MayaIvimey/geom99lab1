0d040c7b1ced6e47f9ec2711e651ebd29cf883d6
MayaIvimey <146374490+MayaIvimey@users.noreply.github.com>
2024-01-25 19:56:17 -0500

    Update story.js

--- story/story.js
+++ story/story.js
@@ -29,13 +29,13 @@ and https://stackoverflow.com/questions/3110020/google-maps-api-v3-no-labels*/
     [{lat: 44.2282385680397, lng: -76.4959873678649},  
      "The Library Café","timhortonsresize.png"],
     [{lat: 44.2259466138417, lng: -76.4913101029787},  
-     "Tim Hortons", icon: "timhortonsresize.png"],
+     "Tim Hortons","timhortonsresize.png"],
     [{lat: 44.2243446625935, lng: -76.4913958693823},  
      "Wallys","timhortonsresize.png"],
     [{lat: 44.2279835998832, lng: -76.4927966741805},  
      "The Tea Room","timhortonsresize.png"],
     [{lat: 44.2290665290748, lng: -76.4948757211298},  
-     "Tim Hortons", icon: "timhortonsresize.png"],
+     "Tim Hortons","timhortonsresize.png"],
     [{lat: 44.2290280899587, lng: -76.494854934011},  
      "Common Ground","timhortonsresize.png"]
   ];
