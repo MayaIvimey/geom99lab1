95a84aa5e323c0b3b13af25f8dbc4ee6bc077248
MayaIvimey <146374490+MayaIvimey@users.noreply.github.com>
2024-01-25 21:58:23 -0500

    Update story.js

--- story/story.js
+++ story/story.js
@@ -27,17 +27,17 @@ and https://stackoverflow.com/questions/3110020/google-maps-api-v3-no-labels*/
     [{lat: 44.2281801650096, lng: -76.4942129018176},  
      "Starbucks","starbucks.png"],
     [{lat: 44.2282385680397, lng: -76.4959873678649},  
-     "The Library Café","timhortonsresize.png"],
+     "The Library Café","coffee.png"],
     [{lat: 44.2259466138417, lng: -76.4913101029787},  
      "Tim Hortons","timhortonsresize.png"],
     [{lat: 44.2243446625935, lng: -76.4913958693823},  
-     "Wallys","timhortonsresize.png"],
+     "Wallys","coffee.png"],
     [{lat: 44.2279835998832, lng: -76.4927966741805},  
-     "The Tea Room","timhortonsresize.png"],
+     "The Tea Room","coffee.png"],
     [{lat: 44.2290665290748, lng: -76.4948757211298},  
      "Tim Hortons","timhortonsresize.png"],
     [{lat: 44.2290280899587, lng: -76.494854934011},  
-     "Common Ground","cogro.png"]
+     "Common Ground","coffee.png"]
   ];
 
   const infoWindow = new google.maps.InfoWindow();
