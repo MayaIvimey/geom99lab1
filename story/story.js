08fc19afb7803eb47832fd72e86e1082929e0c0f
MayaIvimey <146374490+MayaIvimey@users.noreply.github.com>
2024-01-25 19:52:01 -0500

    Update story.js

--- story/story.js
+++ story/story.js
@@ -19,34 +19,34 @@ and https://stackoverflow.com/questions/3110020/google-maps-api-v3-no-labels*/
 
    const locations = [
     [{ lat: 44.223620329676336, lng: -76.4992960158313 },
-      "Location 21"],
+      "Location 21","timhortonsresize.png"],
     [{ lat: 44.22535274880164, lng: -76.49863267430514 },
-      "The Lazy Scholar"],
+      "The Lazy Scholar","timhortonsresize.png"],
     [{lat: 44.2280878986305, lng: -76.4978285248609},  
-     "Starbucks"],
+     "Starbucks","timhortonsresize.png"],
     [{lat: 44.2281801650096, lng: -76.4942129018176},  
-     "Starbucks"],
+     "Starbucks","timhortonsresize.png"],
     [{lat: 44.2282385680397, lng: -76.4959873678649},  
-     "The Library Café"],
+     "The Library Café","timhortonsresize.png"],
     [{lat: 44.2259466138417, lng: -76.4913101029787},  
-     "Tim Hortons"],
+     "Tim Hortons", icon: "timhortonsresize.png"],
     [{lat: 44.2243446625935, lng: -76.4913958693823},  
-     "Wallys"],
+     "Wallys","timhortonsresize.png"],
     [{lat: 44.2279835998832, lng: -76.4927966741805},  
-     "The Tea Room"],
+     "The Tea Room","timhortonsresize.png"],
     [{lat: 44.2290665290748, lng: -76.4948757211298},  
-     "Tim Hortons"],
+     "Tim Hortons", icon: "timhortonsresize.png"],
     [{lat: 44.2290280899587, lng: -76.494854934011},  
-     "Common Ground"]
+     "Common Ground","timhortonsresize.png"]
   ];
 
   const infoWindow = new google.maps.InfoWindow();
-  locations.forEach(([position, title], i) => {
+  locations.forEach(([position, title, icon], i) => {
     const marker = new google.maps.Marker({
       position,
       map,
-      title: `${title}`,
-      label: `${i + 1}`,
+      icon,
+      title: `${title}`, 
       optimized: false,
     });
      marker.addListener("click", () => {
