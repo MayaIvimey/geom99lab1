0e419c4c15b999c5f01927717cc3603abbbaa6ba
MayaIvimey <146374490+MayaIvimey@users.noreply.github.com>
2024-01-25 21:28:34 -0500

    Update story.js

--- story/story.js
+++ story/story.js
@@ -23,9 +23,9 @@ and https://stackoverflow.com/questions/3110020/google-maps-api-v3-no-labels*/
     [{ lat: 44.22535274880164, lng: -76.49863267430514 },
       "The Lazy Scholar","timhortonsresize.png"],
     [{lat: 44.2280878986305, lng: -76.4978285248609},  
-     "Starbucks","timhortonsresize.png"],
+     "Starbucks","starbucks.png"],
     [{lat: 44.2281801650096, lng: -76.4942129018176},  
-     "Starbucks","timhortonsresize.png"],
+     "Starbucks","starbucks.png"],
     [{lat: 44.2282385680397, lng: -76.4959873678649},  
      "The Library Café","timhortonsresize.png"],
     [{lat: 44.2259466138417, lng: -76.4913101029787},  
@@ -56,7 +56,98 @@ and https://stackoverflow.com/questions/3110020/google-maps-api-v3-no-labels*/
       });   
     });
 
- 
+/* inset map from https://developers.google.com/maps/documentation/javascript/examples/inset-map#maps_inset_map-javascript */ 
+overview = new google.maps.Map(document.getElementById("overview"), {
+    center: { lat: 44.226000090936395, lng: -76.49499549568613 }, 
+    zoom: 9,
+   /*Javascript inspired by the night mode script 
+  https://developers.google.com/maps/documentation/javascript/examples/style-array#maps_style_array-javascript
+  */
+      styles: [
+      { elementType: "geometry", stylers: [{ color: "#242f3e" }] },
+      { elementType: "labels.text.stroke", stylers: [{ color: "#242f3e" }] },
+      { elementType: "labels.text.fill", stylers: [{ color: "#746855" }] },
+      {
+        featureType: "administrative.locality",
+        elementType: "labels.text.fill",
+        stylers: [{ color: "#d59563" }],
+      },
+      {
+        featureType: "poi",
+        elementType: "labels.text.fill",
+        stylers: [{ color: "#d59563" }],
+      },
+      {
+        featureType: "poi.park",
+        elementType: "geometry",
+        stylers: [{ color: "#263c3f" }],
+      },
+      {
+        featureType: "poi.park",
+        elementType: "labels.text.fill",
+        stylers: [{ color: "#6b9a76" }],
+      },
+      {
+        featureType: "road",
+        elementType: "geometry",
+        stylers: [{ color: "#38414e" }],
+      },
+      {
+        featureType: "road",
+        elementType: "geometry.stroke",
+        stylers: [{ color: "#212a37" }],
+      },
+      {
+        featureType: "road",
+        elementType: "labels.text.fill",
+        stylers: [{ color: "#9ca5b3" }],
+      },
+      {
+        featureType: "road.highway",
+        elementType: "geometry",
+        stylers: [{ color: "#746855" }],
+      },
+      {
+        featureType: "road.highway",
+        elementType: "geometry.stroke",
+        stylers: [{ color: "#1f2835" }],
+      },
+      {
+        featureType: "road.highway",
+        elementType: "labels.text.fill",
+        stylers: [{ color: "#f3d19c" }],
+      },
+      {
+        featureType: "transit",
+        elementType: "geometry",
+        stylers: [{ color: "#2f3948" }],
+      },
+      {
+        featureType: "transit.station",
+        elementType: "labels.text.fill",
+        stylers: [{ color: "#d59563" }],
+      },
+      {
+        featureType: "water",
+        elementType: "geometry",
+        stylers: [{ color: "#17263c" }],
+      },
+      {
+        featureType: "water",
+        elementType: "labels.text.fill",
+        stylers: [{ color: "#515c6d" }],
+      },
+      {
+        featureType: "water",
+        elementType: "labels.text.stroke",
+        stylers: [{ color: "#17263c" }],
+      },
+    ],
+    disableDefaultUI: true,
+    gestureHandling: "none",
+    zoomControl: true,
+  }); 
+   
 
 }
 
